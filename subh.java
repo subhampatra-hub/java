225c0d2e810b8d6b857542c645c71651c4aa876e
subham patra <subhampatra8260@gmailcom>
2025-02-18 23:49:37 +0530

    first commit

---
+++ subh.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class subh {
+    public static void main(String[]args){
+        
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a sentence:-");
+        String sen=sc.nextLine();
+        int count=0;
+        for(int i=0;i<sen.length();i++){
+            if(sen.charAt(i)==' '){
+                count++;
+            }
+        }
+        System.out.println("The total sapace is:"+count);
+        kkk
+        
+        
+
+        
+        
+    }
+   
+    
+    
+}
